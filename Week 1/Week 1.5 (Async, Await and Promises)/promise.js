5e134a4266317313f8855b2764c30bdb9a2fc64f
Vishal Panwar <119125256+vishallpanwar@users.noreply.github.com>
2024-05-24 22:50:03 +0530

    Add files via upload

---
+++ Week 1/Week 1.5 (Async, Await and Promises)/promise.js
@@ -0,0 +1,13 @@
+// Phle one chlega, fir three, fir two kyuki two me timer hai and jbtk timer chlega tbtk compiler baki kaam complete krega
+
+console.log("One");
+
+
+
+setTimeout(function(){
+    console.log("Two");
+},5000);
+
+
+
+console.log("Three");
